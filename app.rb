302347d67c790fe4ab50447a39fc3af2e8423fef
Kelton Person <kelton.person@gmail.com>
2013-01-10 20:02:32 -0500

    bla bla bla

--- app.rb
+++ app.rb
@@ -13,8 +13,25 @@ get "/" do
   # serialized_object = YAML::dump(a)
   #puts YAML::load(serialized_object)
   #=end
+  @js = ["hello.js"]
   @fonts = ["arial","helvetica","tahoma","GOthAm",'jim'];
   haml :test
+end
+
+
+
+before do
+end
+
+helpers do
+  def load_js(js_files)
+    my_js = ""
+    js_files.each do |file|
+      my_js.concat("<script type='text/javascript' src='#{file}'></script>\n")
+    end
+    my_js
+
+  end
 end
 
 before do
